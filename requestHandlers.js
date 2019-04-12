b1171c0b20aa339d6a97af584980c17cba0fe167
Lexel Yu <Lexel_Yu@asus.com>
2019-04-12 17:35:45 +0800

    Update requestHandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,7 +5,7 @@ const exec = require('child_process').exec;
 function start(response) {
   console.log('Request handler \'start\' was called.');
 
-  exec('dir', function(error, stdout, stderr) {
+  exec('ls -l', function(error, stdout, stderr) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello start\n');
     response.write(stdout);
