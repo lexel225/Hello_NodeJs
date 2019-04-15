ea99ce891d23ce3823605dcf9f4bfcdbf56bd7bf
Lexel Yu <Lexel_Yu@asus.com>
2019-04-15 14:33:02 +0800

    Upgrade to POST text version

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,26 +1,46 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable require-jsdoc */
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
 
-function start(response) {
+function start(response, postData) {
   console.log('Request handler \'start\' was called.');
 
+  const body = '<html>'+
+  '<head>'+
+  '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+  '</head>'+
+  '<body>'+
+  '<form action="/upload" method="post">'+
+  '<textarea name="text" rows="20" cols="60"></textarea>'+
+  '<input type="submit" value="Submit text" />'+
+  '</form>'+
+  '</body>'+
+  '</html>';
+
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(body);
+  response.end();
+/**
   exec('ls -l', function(error, stdout, stderr) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello start\n');
     response.write(stdout);
     response.end();
   });
+**/
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log('Request handler \'upload\' was called.');
+
+  const querystring = require('querystring');
+
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello upload\n');
+  response.write('You send: ' + querystring.parse(postData).text);
   response.end();
 }
 
-function lexel(response) {
+function lexel(response, postData) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hi, Lexel\n');
   response.end();
--- server.js
+++ server.js
@@ -11,10 +11,21 @@ const port = process.env.PORT || 8888;
  */
 function start(route, handle) {
   function onRequest(request, response) {
+    let postData = '';
     const pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(handle, pathname, response);
+    request.setEncoding('utf8');
+
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('Received POST data chunk \''+ postDataChunk + '\'.');
+    });
+
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData);
+    });
+    // route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(port);
